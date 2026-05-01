9d82e51b8ba374efa5ecfa3103fb55bd06dab100
LBYHKKDCYX <3689395508@qq.com>
2026-05-01 22:24:51 +0800

    完善PWA功能

---
+++ static/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'aviation-helper-v1';
+
+const ASSETS = [
+  '/',
+  '/static/css/style.css',
+  '/static/android-chrome-192x192.png',
+  '/static/android-chrome-512x512.png',
+  '/static/favicon.ico'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
